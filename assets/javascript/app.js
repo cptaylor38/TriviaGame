3e29abc58ef0f401d563bfa010644ada9716f79e
Corbin Taylor <cptaylor38@yahoo.com>
2019-02-24 04:12:29 -0500

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,27 @@
 var round = 0;
-var roundCompleted = setInterval(chartCreate, 1000 * 10);
+var intervalId;
+var number;
+var clockRunning = false;
+var intervalId;
 
+function Quote(q, a, b, c, d, correct, img) {
+    this.q = q;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    this.correct = correct;
+    this.imgURL = img;
+}
 
+var firstQuote = new Quote("'God help us, we're in the hands of engineers'", 'What is: Jurassic Park', 'What is: Independence Day', 'What is: Powder', 'What is: The Big Chill', 'a', 'assets/images/jurassicPark.jpg');
+var secondQuote = new Quote("'That's what's great about the outdoors, it's one giant toilet'", 'What is: Jurassic Park, The Lost World', 'What is: Independence Day', 'What is: The Big Chill', 'What is: Jurassic Park', 'c', 'assets/images/bigChill.jpg');
+var thirdQuote = new Quote("'We've come to a point where our technology has surpassed our humanity, but I hope, someday, our humanity will surpass our technology'", 'What is: Jurassic Park, The Lost World', 'What is: Independence Day',
+    'What is: Powder', 'What is: Vibes', 'c', 'assets/images/powder.jpg');
+var fourthQuote = new Quote("'Oohh, Aahh, that's how all of this starts, but then later there's the running and screaming'", 'What is: Jurassic Park, The Lost World', 'What is: Independence Day', 'What is: Powder',
+    'What is: The Big Chill', 'a', 'assets/images/lostWorld.jpg');
 
-$("#play").on("click", function () {  //calling play function at play button onclick event
+$("#play").on("click", function () {
 
     $("#gameMenu").empty();
     $(this).remove();
@@ -12,9 +30,10 @@ $("#play").on("click", function () {  //calling play function at play button onc
 })
 
 var chartCreate = function () {
+
     $('#game').empty();
     $('#questionBox').empty();
-
+    clearInterval(intervalId);
     $titleRow = $("<h1>").text("Jeffpardy");
     $titleRow.css('width', '100%', 'height', '100%');
     $("#game").prepend($titleRow);
@@ -30,65 +49,45 @@ var chartCreate = function () {
         }
 
         $("#game").append($topBoxes);
-
     }
+
 
 
 }
 
 
 $(document).on('click', 'button', function () {
+    console.log('onclick event 1');
     if ($(this).attr('id') == 'triviaButton') {
-        console.log('function called');
         $('#game').empty();
         if (round == 0) {
-            round++;
             askQuote(firstQuote);
         }
         else if (round == 1) {
-            round++;
             askQuote(secondQuote);
         }
         else if (round == 2) {
-            round++;
             askQuote(thirdQuote);
         }
         else if (round == 3) {
-            round++;
             askQuote(fourthQuote);
-        }
-        else {
-            $('#game').empty();
-            $('#game').text("Thanks for playing!");
         }
     }
 })
 
-function Quote(q, a, b, c, d, correct, img) {
-    this.q = q;
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.d = d;
-    this.correct = correct;
-    this.imgURL = img;
-}
-
-var firstQuote = new Quote("'God help us, we're in the hands of engineers'", 'Jurassic Park', 'Independence Day', 'Powder', 'The Big Chill', 'a', 'assets/images/jurassicPark.jpg');
-var secondQuote = new Quote("'That's what's great about the outdoors, it's one giant toilet'", 'Jurassic Park: The Lost World', 'Independence Day', 'The Big Chill', 'Jurassic Park', 'c', 'assets/images/bigChill.jpg');
-var thirdQuote = new Quote("'We've come to a point where our technology has surpassed our humanity, but I hope, someday, our humanity will surpass our technology'", 'Jurassic Park: The Lost World', 'Independence Day',
-    'Powder', 'Vibes', 'c', 'assets/images/powder.jpg');
-var fourthQuote = new Quote("'Oohh, Aahh, that's how all of this starts, but then later there's the running and screaming'", 'Jurassic Park: The Lost World', 'Independence Day', 'Powder',
-    'The Big Chill', 'a', 'assets/images/lostWorld.jpg');
-
-
 askQuote = function (currentQuote) {
+    $('#questionBox').empty();
+    clearInterval(intervalId);
+    console.log('askQuote');
+    number = 15;
+    round++;
     $quoteBox = $('<p>').text(currentQuote.q);
     $quoteBox.attr('id', 'chosenQuote');
     $answerA = $('<button>').text(currentQuote.a);
     $answerB = $('<button>').text(currentQuote.b);
     $answerC = $('<button>').text(currentQuote.c);
     $answerD = $('<button>').text(currentQuote.d);
+    $timer = $('<h1>').text(number);
 
     $answerA.attr({
         'id': 'a',
@@ -107,25 +106,37 @@ askQuote = function (currentQuote) {
         'class': 'answerButton'
     });
 
-    $('#questionBox').append($quoteBox, $answerA, $answerB, $answerC, $answerD);
+    $('#questionBox').append($quoteBox, $answerA, $answerB, $answerC, $answerD, $timer);
 
     $(document).on('click', 'button', function () {
+        console.log('onclick 2');
         if ($(this).attr('class') == 'answerButton') {
             if ($(this).attr('id') == currentQuote.correct) {
                 rightAnswer(currentQuote.imgURL)
             }
             else {
                 wrongAnswer();
-
             }
-
-        }
-        else {
-            console.log('condition not met');
         }
     })
-}
 
+    timer = function () {
+        console.log('timer');
+        number--;
+        $timer.text(number);
+
+        if (number === 0 && round === 4) {
+            gameOver();
+        }
+        else if (number === 0 && round < 4) {
+            chartCreate();
+        }
+
+
+    }
+
+    intervalId = setInterval(timer, 1000);
+}
 
 rightAnswer = function (currentQuote) {
     $("#questionBox").empty();
@@ -139,15 +150,12 @@ rightAnswer = function (currentQuote) {
     $message = $('<h1>').text('You got it!\n');
     $message.append($quoteImage);
     $('#questionBox').append($message);
-    if (round === 3) {
+    if (round === 4) {
         setTimeout(gameOver, 1000 * 3);
-
     }
     else {
         setTimeout(chartCreate, 1000 * 3);
     }
-
-
 }
 
 wrongAnswer = function () {
@@ -161,22 +169,24 @@ wrongAnswer = function () {
     $message = $('<h1>').text('Wrong Answer. \n"Checkmate" - Independence Day');
     $message.append($quoteImage);
     $('#questionBox').append($message);
-    if (round === 3) {
+    if (round === 4) {
         setTimeout(gameOver, 1000 * 3);
-
     }
     else {
         setTimeout(chartCreate, 1000 * 3);
     }
-
 }
 
+
 var gameOver = function () {
+    console.log('gameOver');
     $('#game').empty();
     $('#questionBox').empty();
     $endGame = $('<h1>').text('Thanks for playing!');
     $('#game').append($endGame);
 }
+
+
 
 
 
